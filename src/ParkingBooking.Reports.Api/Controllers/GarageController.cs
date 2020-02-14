045b8993f1ee63e3186b4cc8540aa45ef364bf3e
Luiz Fernando Dias Rezende <luizdias.inf@gmail.com>
2020-02-14 12:07:52 +0100

    Adding assets and presentation

---
+++ src/ParkingBooking.Reports.Api/Controllers/GarageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingBooking.Reports.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class GarageController : ControllerBase
+    {
+        [HttpGet("{garageId}")]
+        public async Task<IActionResult> Get(
+            Guid garageId, 
+            DateTime startDate, 
+            DateTime endDate)
+        {            
+            return Ok(new 
+            {
+                data = new object[] { },
+                message = "Feature not available yet"
+            });
+        }
+    }
+}
